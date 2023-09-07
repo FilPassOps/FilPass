2ceb20fb379b2efbbbbcb986f797019c79b73053
Wellington Santos <wellington.santos@ae.studio>
2023-09-07 15:56:05 -0300

    - Changing otp component

--- components/Authentication/SecurityCodeModal.tsx
+++ components/Authentication/SecurityCodeModal.tsx
@@ -3,7 +3,7 @@ import { Button } from 'components/shared/Button'
 import { useState } from 'react'
 import { DateTime } from 'luxon'
 import CountDownTimer from './CountDownTimer'
-import VerificationInput from 'react-verification-input'
+import OtpInput from 'react-otp-input'
 
 interface SecurityCodeModalProps {
   open?: boolean
@@ -25,12 +25,14 @@ export const SecurityCodeModal = ({
   const [submitErrors, setSubmitErrors] = useState<any>()
   const [loading, setLoading] = useState(false)
   const [blocked, setBlocked] = useState(false)
+  const [code, setCode] = useState('')
 
   const handleClose = () => {
     setSubmitErrors(null)
     setLoading(false)
     onClose(false)
     setBlocked(false)
+    setCode('')
   }
 
   const handleComplete = async (value: string) => {
@@ -39,6 +41,14 @@ export const SecurityCodeModal = ({
     if (error) {
       return setSubmitErrors(error.message)
     }
+  }
+
+  const handleOnChangeCode = (value: string) => {
+    setSubmitErrors(null)
+    if (value.length === 4) {
+      handleComplete(value)
+    }
+    setCode(value)
   }
 
   const handleResend = async () => {
@@ -65,21 +75,16 @@ export const SecurityCodeModal = ({
         Please enter a 4-digit security code that was sent to you email address <span className="text-indigo-600">{email}.</span> The code
         is valid for 10 minutes
       </p>
-      <div className={'flex flex-row items-center justify-center'}>
-        <VerificationInput
-          autoFocus={true}
-          onComplete={handleComplete}
-          placeholder={''}
-          validChars="0-9"
-          length={4}
-          classNames={{
-            character: 'border border-g rounded-md outline-1 border-gray-400',
-            container: 'flex flex-row gap-5',
-            characterInactive: 'bg-white',
-            characterSelected: 'border-indigo-600',
-          }}
-        />
-      </div>
+      <OtpInput
+        shouldAutoFocus={true}
+        inputType="number"
+        containerStyle={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}
+        inputStyle={{ width: '3rem', height: '3rem', borderRadius: '0.375rem', border: '1px solid #D1D5DB', fontSize: '2rem' }}
+        value={code}
+        onChange={handleOnChangeCode}
+        numInputs={4}
+        renderInput={props => <input {...props} />}
+      />
       <p className="text-center text-sm text-red-500 mt-3">{submitErrors}</p>
       <div className="flex flex-row items-center justify-center mt-8 pb-4">
         <p className="mr-3 text-sm text-gray-500">Didn&apos;t get an email?</p>
